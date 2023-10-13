d0068bff9e2ab2bbf834883dd15cce9d88511eba
manumafe98 <manumaxerapro@gmail.com>
2023-10-13 19:43:09 -0300

    Adding user functionality so when clicks a button the label displays the correct image

--- rock_paper_scissors/src/gui/Gui.java
+++ rock_paper_scissors/src/gui/Gui.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.util.Random;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -14,6 +15,9 @@ public class Gui {
     private ImageIcon scissorsImage;
     private Random random;
     private ImageIcon[] imageArray;
+    private JButton rockButton;
+    private JButton paperButton;
+    private JButton scissorsButton;
     
     public Gui() {
         frame = new JFrame("Rock, Paper, Scissors");
@@ -23,25 +27,48 @@ public class Gui {
         paperImage = new ImageIcon("E:/Java Projects/rock_paper_scissors/images/paper.jpeg");
         scissorsImage = new ImageIcon("E:/Java Projects/rock_paper_scissors/images/scissors.jpeg");
         imageArray = new ImageIcon[3];
+
+        rockButton = new JButton("Rock");
+        paperButton = new JButton("Paper");
+        scissorsButton = new JButton("Scissors");
         
         imageArray[0] = rockImage;
         imageArray[1] = paperImage;
         imageArray[2] = scissorsImage;
 
         randomImageLabel = new JLabel();
+        userImageLabel = new JLabel();
 
     }
 
     public void createGui() {
-        randomImageLabel.setBounds(0, 0, 500, 500);
-        randomImageLabel.setIcon(imageArray[random.nextInt(3)]);
+        //randomImageLabel.setBounds(0, 0, 500, 500);
+        //randomImageLabel.setIcon(imageArray[random.nextInt(3)]);
+        userImageLabel.setBounds(0, 0, 500, 500);
+
+        rockButton.setBounds(0, 500, 100, 50);
+        rockButton.setFocusable(false);
+        rockButton.addActionListener(e -> userImageLabel.setIcon(imageArray[0]));
+
+        paperButton.setBounds(120, 500, 100, 50);
+        paperButton.setFocusable(false);
+        paperButton.addActionListener(e -> userImageLabel.setIcon(imageArray[1]));
+
+        scissorsButton.setBounds(240, 500, 100, 50);
+        scissorsButton.setFocusable(false);
+        scissorsButton.addActionListener(e -> userImageLabel.setIcon(imageArray[2]));
+
 
         frame.setLayout(null);
         frame.setSize(1000, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        frame.add(randomImageLabel);
+        //frame.add(randomImageLabel);
+        frame.add(userImageLabel);
+        frame.add(rockButton);
+        frame.add(paperButton);
+        frame.add(scissorsButton);
 
     }
 }
