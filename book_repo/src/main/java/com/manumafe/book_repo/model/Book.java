1995fa13ed684558307d97c4b0e0e8deed3decf5
manumafe98 <manumaxerapro@gmail.com>
2023-10-18 17:50:34 -0300

    Adding bookStatus variable to book model

--- book_repo/src/main/java/com/manumafe/book_repo/model/Book.java
+++ book_repo/src/main/java/com/manumafe/book_repo/model/Book.java
@@ -8,36 +8,23 @@ import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class Book {
-    
+
     @Id
-    @SequenceGenerator(
-        name = "book_id_sequence",
-        sequenceName = "book_id_sequence",
-        allocationSize = 1
-    )
-    @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "book_id_sequence"
-    )
+    @SequenceGenerator(name = "book_id_sequence", sequenceName = "book_id_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_id_sequence")
     private Integer id;
     private String bookName;
     private String bookAuthor;
-    
+    private String bookStatus;
+
     public Book() {
     }
 
-    public Book(Integer id, String bookName, String bookAuthor) {
+    public Book(Integer id, String bookName, String bookAuthor, String bookStatus) {
         this.id = id;
         this.bookName = bookName;
         this.bookAuthor = bookAuthor;
-    }
-    
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+        this.bookStatus = bookStatus;
     }
 
     public String getBookName() {
@@ -55,7 +42,21 @@ public class Book {
     public void setBookAuthor(String bookAuthor) {
         this.bookAuthor = bookAuthor;
     }
-    
+
+    public String getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(String bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", bookName=" + bookName + ", bookAuthor=" + bookAuthor + ", bookStatus=" + bookStatus
+                + "]";
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -63,9 +64,10 @@ public class Book {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((bookName == null) ? 0 : bookName.hashCode());
         result = prime * result + ((bookAuthor == null) ? 0 : bookAuthor.hashCode());
+        result = prime * result + ((bookStatus == null) ? 0 : bookStatus.hashCode());
         return result;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -90,11 +92,11 @@ public class Book {
                 return false;
         } else if (!bookAuthor.equals(other.bookAuthor))
             return false;
+        if (bookStatus == null) {
+            if (other.bookStatus != null)
+                return false;
+        } else if (!bookStatus.equals(other.bookStatus))
+            return false;
         return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Book [id=" + id + ", bookName=" + bookName + ", bookAuthor=" + bookAuthor + "]";
     }
 }
