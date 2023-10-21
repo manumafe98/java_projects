7da137f248e19ac4fab80833a6b65ce3341fa24e
manumafe98 <manumaxerapro@gmail.com>
2023-10-20 21:01:12 -0300

    Removing book repo from java projects cause it was creating into a new own repository

--- book_repo/src/main/java/com/manumafe/book_repo/exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.manumafe.book_repo.exceptions;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- book_repo/src/main/java/com/manumafe/book_repo/model/BookStatus.java
+++
@@ -1,7 +0,0 @@
-package com.manumafe.book_repo.model;
-
-public enum BookStatus {
-    WANT_TO_READ,
-    READING,
-    READ
-}
